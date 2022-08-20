ee5e3e275cdaeff544a8d565a50f1d4ea6f1a328
Yogesh Sharma <110836592+yogeshshiva@users.noreply.github.com>
2022-08-20 22:58:10 +0530

    Add files via upload

---
+++ week.py
@@ -0,0 +1,17 @@
+num=int(input("enter your number between 1 to 7."))
+if (num==1):
+    print("sunday")
+elif (num==2):
+    print("monday")
+elif (num==3):
+    print("tuesday")
+elif (num==4):
+    print("wednesday")
+elif (num==5):
+    print("thrusday")
+elif (num==6):
+    print("friday")
+elif (num==7):
+    print("saturaday")
+else:
+    print("enter number only 1 to 7.")
